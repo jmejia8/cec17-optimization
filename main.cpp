c210d135f2be816d0d19be321ce4395fc79a66f6
Jesús Mejía <jesusmejded@gmail.com>
2017-03-01 16:34:35 -0600

    Corrección de errores de lógica

--- main.cpp
+++ main.cpp
@@ -35,56 +35,23 @@ int main()
 	n = 10;
 
 	// iteration number
-	iter = 1000;
+	iter = 10000;
+
+	// function number
+	func_num = 1;
 
 	x = allocSperms(m, n);
 	f = allocSperms(m, 1);
 
-	setSpermsPosition(x, n, m);
-	evaluateSperms(x, f, n, m, 1);
-	
-	optim(x, f, m, n, 1, iter);
-	return 0;
+	setSpermsPosition(x, m, n);
+	evaluateSperms(x, f, m, n, func_num);
 
-	for (i = 0; i < 30; i++)
-	{
-		func_num=i+1;
-		sprintf(FileName, "input_data/shift_data_%d.txt", func_num);
-		fpt = fopen(FileName,"r");
-		if (fpt==NULL)
-		{
-			printf("\n Error: Cannot open input file for reading \n");
-		}
-		
-		if (x==NULL)
-			printf("\nError: there is insufficient memory available!\n");
-
-		for(k=0;k<n;k++)
-		{
-				fscanf(fpt,"%lf",&x[k]);
-				/*printf("%lf\n",x[k]);*/
-		}
-
-		fclose(fpt);
-
-			for (j = 0; j < n; j++)
-			{
-				x[1*n+j]=0.0;
-				/*printf("%lf\n",x[1*n+j]);*/
-			}
-		
-		
-		for (k = 0; k < 1; k++)
-		{
-			cec17_test_func(x, f, n,m,func_num);
-			for (j = 0; j < 2; j++)
-			{
-				printf(" f%d(x[%d]) = %lf,",func_num,j+1,f[j]);
-			}
-			printf("\n");
-		}
-	
+	for (int i = 0; i < m; ++i) {
+		printf("%lf\t", f[i]);
 	}
+
+	optim(x, f, m, n, func_num, iter);
+
 	free(x);
 	free(f);
 	free(y);
--- optim.hpp
+++ optim.hpp
@@ -1,13 +1,13 @@
 // Porcentaje de la longitud del paso por interación
-#define STEP_P 0.1
-#define H 0.0001
+#define STEP_P 1e-6
+#define H 1e-6
 
 double partial (double* x, int sperm, int xi, int n, int func_num){
 	int i, m = 2;
 	double* x_tmp = allocSperms(m, n);
 	double* f_tmp = allocSperms(m, 1);
 
-	int a = sperm*n;
+	int a = sperm;
 
 	for (i = 0; i < n; ++i){
 		x_tmp[i] = x[a + i];
@@ -19,6 +19,7 @@ double partial (double* x, int sperm, int xi, int n, int func_num){
 	cec17_test_func(x_tmp, f_tmp, n, m, func_num);
 
 	double df = (f_tmp[0] - f_tmp[1]) / H;
+	// printf(">>>> >>> %lf\n", df);
 
 	free(x_tmp);
 	free(f_tmp);
@@ -27,22 +28,31 @@ double partial (double* x, int sperm, int xi, int n, int func_num){
 
 }
 
-void nextPosition (double* x, int sperm, int egg, int n, int func_num){
+void nextPosition (double* x, int sperm, int egg, int n, int func_num, int iter){
 	int i; double df;
+	// printf("position %lf\n", x[0]);
+
 
 	for (i = 0; i < n; ++i) {
+		if (x[sperm + i] > 100 || x[sperm + i] < -100)
+			continue;
+
 		df = partial(x, sperm, i, n, func_num);
 
-		x[sperm + i] += STEP_P * df  -  (x[egg] - x[i]);
+		x[sperm + i] += STEP_P * df;
+
+		// if (iter > 10) {
+		// 	x[sperm + i]  += 0.1*(x[egg + i] - x[sperm + i]);
+		// }
 	}
 
 }
 
-int iterate(double* x, double* f, int egg, int m, int n, int func_num){
+int iterate(double* x, double* f, int egg, int m, int n, int func_num, int iter){
 	int i;
 	for (i = 0; i < m; ++i) {
-
-		nextPosition(x, i*n, egg*n, n, func_num);
+		// printf("iter %lf\n", x[0]);
+		nextPosition(x, i*n, egg*n, n, func_num, iter);
 		cec17_test_func(x, f, n, m, func_num);
 		
 		if (egg != i && f[i] < f[egg])
@@ -54,20 +64,24 @@ int iterate(double* x, double* f, int egg, int m, int n, int func_num){
 
 void optim(double* x, double* f, int m,  int n, int func_num, int iter) {
 	int i, egg = 0;
-
 	int old = -1;
+
+	printf("iter \t egg \t f_val\n");
+	printf("---------------------------------------\n");
 	for (i = 0; i < iter; ++i) {
-		egg = iterate(x, f, egg, m, n, func_num);
+		egg = iterate(x, f, egg, m, n, func_num, i);
 		if (egg != old) {
-			printf("%d %lf \n", egg, f[egg]);
+			printf("%d \t %d \t %.20lf \n", i, egg, f[egg]);
 		}
 
 		old = egg;
 	}
 
-	printf(">---->>>>>>>>---------->>>>>>\n");
-	for (int i = 0; i < m; ++i) {
-		printf("%lf\n", f[i]);
+	egg *= n;
+	printf("\n\n_------------------------------------------\n");
+	for (int i = 0; i < n; ++i)
+	{
+		printf("%lf\n", x[egg + i]);
 	}
 
 }
